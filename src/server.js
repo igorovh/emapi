81e6158a3c335d52e877e0d2b7a548573de2618d
α <animekgaming@gmail.com>
2021-08-30 22:57:12 +0200

    Connect to redis server given in config

--- src/server.js
+++ src/server.js
@@ -3,9 +3,17 @@ import fs from 'fs';
 import redis from 'redis';
 import util from 'util';
 
-export const client = redis.createClient();
-client.hget = util.promisify(client.hget);
-
 export const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+export const client = redis.createClient({
+    host: config.redis.host,
+    port: config.redis.port
+});
+if(config.redis.password.length > 0) {
+    console.info('Logging to redis with password...');
+    client.auth(config.redis.password);
+}
+
+client.hget = util.promisify(client.hget);
 
 app.listen(config.port, () => console.log(`Service running at port ${config.port}`));
