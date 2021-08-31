56832ef92a5d0ac2cc26bb859e57268e7d88c56b
α <animekgaming@gmail.com>
2021-08-31 23:44:53 +0200

    Fixing FFZ parser

--- src/parsers/ffzparser.js
+++ src/parsers/ffzparser.js
@@ -4,16 +4,18 @@ export function parse(json) {
     const emotes = [];
     if(json && json.status !== 404) {
         const sets = json.sets;
-        Object.keys(sets).forEach(set => {
-            sets[set].emoticons.forEach(emoteJson => {
-                const urls = {
-                    '1x': emoteJson.urls['1'],
-                    '2x': emoteJson.urls['2'],
-                    '4x': emoteJson.urls['4']
-                }
-                emotes.push(new Emote(emoteJson.name, urls));
-            });
-        })
+        if(sets !== undefined) {
+            Object.keys(sets).forEach(set => {
+                sets[set].emoticons.forEach(emoteJson => {
+                    const urls = {
+                        '1x': emoteJson.urls['1'],
+                        '2x': emoteJson.urls['2'],
+                        '4x': emoteJson.urls['4']
+                    }
+                    emotes.push(new Emote(emoteJson.name, urls));
+                });
+            })
+        }
     }
     return emotes;
 }
