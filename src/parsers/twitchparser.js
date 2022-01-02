b5d12385ac39921e3d4239321f981d27e869a890
animekkk <animekgaming@gmail.com>
2022-01-02 18:05:59 +0100

    Fix for gif emotes

--- src/parsers/twitchparser.js
+++ src/parsers/twitchparser.js
@@ -5,10 +5,11 @@ export function parse(json) {
     if(json && json.status !== 404 && json.data && json.data.length > 0) {
         json.data.forEach(emoteJson => {
             const urls = {
-                '1x': emoteJson.images['url_1x'],
-                '2x': emoteJson.images['url_2x'],
-                '4x': emoteJson.images['url_4x'],
+                '1x': emoteJson.images['url_1x'].replace('/static/', '/default/'),
+                '2x': emoteJson.images['url_2x'].replace('/static/', '/default/'),
+                '4x': emoteJson.images['url_4x'].replace('/static/', '/default/'),
             }
+            console.log(urls);
             emotes.push(new Emote(emoteJson.name, urls));
         });
     }
